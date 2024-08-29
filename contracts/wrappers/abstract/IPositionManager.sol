71f9a29c799b03fc9a01f62ee95206842263c064
Navina Lang <81016364+langnavina97@users.noreply.github.com>
2024-08-29 13:26:54 +0700

    add missing natspec comments

--- contracts/wrappers/abstract/IPositionManager.sol
+++ contracts/wrappers/abstract/IPositionManager.sol
@@ -50,22 +50,45 @@ interface IPositionManager {
     WrapperFunctionParameters.InitialMintParams memory params
   ) external;
 
+  /**
+   * @notice Increases liquidity in an existing Uniswap V3 position and mints corresponding wrapper tokens.
+   * @param _params Struct containing parameters necessary for adding liquidity and minting tokens.
+   * @dev Handles the transfer of tokens, adds liquidity to Uniswap V3, and mints wrapper tokens proportionate to the added liquidity.
+   */
   function increaseLiquidity(
     WrapperFunctionParameters.WrapperDepositParams memory _params
   ) external;
 
+  /**
+   * @notice Decreases liquidity for an existing Uniswap V3 position and burns the corresponding wrapper tokens.
+   * @param _positionWrapper Address of the position wrapper contract.
+   * @param _withdrawalAmount Amount of wrapper tokens representing the liquidity to be removed.
+   * @param _amount0Min Minimum amount of token0 expected to prevent slippage.
+   * @param _amount1Min Minimum amount of token1 expected to prevent slippage.
+   * @param tokenIn The address of the token to be swapped (input).
+   * @param tokenOut The address of the token to be received (output).
+   * @param amountIn The amount of `tokenIn` to be swapped to `tokenOut`.
+   * @dev Burns wrapper tokens and reduces liquidity in the Uniswap V3 position based on the provided parameters.
+   */
   function decreaseLiquidity(
     address _positionWrapper,
     uint256 _withdrawalAmount,
     uint256 _amount0Min,
     uint256 _amount1Min,
-    // swap params
     address tokenIn,
     address tokenOut,
     uint256 amountIn
   ) external;
 
+  /**
+   * @notice Function to retrieve the asset management configuration contract address.
+   * @return The address of the asset management configuration contract.
+   */
   function assetManagementConfig() external view returns (address);
 
+  /**
+   * @notice Function to retrieve the protocol configuration contract address.
+   * @return The address of the protocol configuration contract.
+   */
   function protocolConfig() external view returns (address);
 }
