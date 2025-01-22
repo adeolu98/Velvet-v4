ba0d670564de67a2f2726869ff0be825bc8ea736
Jay Singh Dhakad <jaysingh20196@gmail.com>
2025-01-22 16:34:10 +0530

    feat: deleted unwanted contract

--- contracts/meta-aggregator/test/MetaAggregatorTestManager.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.17;
-
-import {MetaAggregatorManager} from "../MetaAggregatorManager.sol";
-
-contract MetaAggregatorTestManager is MetaAggregatorManager {
-    constructor(address _metaAggregatorTestSwap) MetaAggregatorManager(_metaAggregatorTestSwap){}
-}
