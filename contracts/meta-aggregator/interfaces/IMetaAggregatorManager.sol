85c1e155ff3739c98a38a2dc1b44ad577278928a
Jay Singh Dhakad <jaysingh20196@gmail.com>
2025-01-22 16:34:21 +0530

    feat: updated interfaces

--- contracts/meta-aggregator/interfaces/IMetaAggregatorManager.sol
+++ contracts/meta-aggregator/interfaces/IMetaAggregatorManager.sol
@@ -2,16 +2,31 @@
 pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-
 interface IMetaAggregatorManager {
-    function swap(
-        IERC20 tokenIn,
-        IERC20 tokenOut,
-        address aggregator,
-        bytes calldata swapData,
-        uint256 amountIn,
-        uint256 minAmountOut,
-        address receiver,
-        bool isDelegate
-    ) external;
+    
+    /// @notice Struct to hold parameters for the ERC20 swap operation
+    /// @param tokenIn The ERC20 token to swap from
+    /// @param tokenOut The ERC20 token to swap to
+    /// @param aggregator The address of the aggregator to use for the swap
+    /// @param swapData Additional data required for the swap
+    /// @param amountIn The amount of tokenIn to swap
+    /// @param minAmountOut The minimum amount of tokenOut expected from the swap
+    /// @param receiver The address that will receive the tokenOut
+    /// @param isDelegate Indicates if the swap is being executed by a delegate
+    /// @param targets Array of addresses to call during the swap for fees
+    /// @param calldataArray Array of calldata for each target for fees
+    struct SwapERC20Params {
+        IERC20 tokenIn;
+        IERC20 tokenOut;
+        address aggregator;
+        bytes swapData;
+        uint256 amountIn;
+        uint256 minAmountOut;
+        address receiver;
+        bool isDelegate;
+        address[] targets;
+        bytes[] calldataArray;
+    }
+
+    function swap(SwapERC20Params calldata params) external;
 }
--- contracts/meta-aggregator/interfaces/IMetaAggregatorSwapContract.sol
+++ contracts/meta-aggregator/interfaces/IMetaAggregatorSwapContract.sol
@@ -2,27 +2,58 @@
 pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-
 interface IMetaAggregatorSwapContract {
-    function swapERC20(
-        IERC20 tokenIn,
-        IERC20 tokenOut,
-        address aggregator,
-        bytes calldata swapData,
-        uint256 amountIn,
-        uint256 minAmountOut,
-        address receiver,
-        bool isDelegate
-    ) external;
 
-    function swapETH(
-        address tokenIn,
-        IERC20 tokenOut,
-        address aggregator,
-        bytes calldata swapData,
-        uint256 amountIn,
-        uint256 minAmountOut,
-        address receiver,
-        bool isDelegate
-    ) external payable;
+    /// @notice Struct to hold parameters for the ETH swap operation
+    /// @param tokenIn The ETH token to swap from
+    /// @param tokenOut The ERC20 token to swap to
+    /// @param aggregator The address of the aggregator to use for the swap
+    /// @param swapData Additional data required for the swap
+    /// @param amountIn The amount of tokenIn to swap
+    /// @param minAmountOut The minimum amount of tokenOut expected from the swap
+    /// @param receiver The address that will receive the tokenOut
+    /// @param isDelegate Indicates if the swap is being executed by a delegate
+    /// @param targets Array of addresses to call during the swap for fees
+    /// @param values Array of values for each target for fees
+    struct SwapETHParams {
+        address tokenIn;
+        IERC20 tokenOut;
+        address aggregator;
+        bytes swapData;
+        uint256 amountIn;
+        uint256 minAmountOut;
+        address receiver;
+        bool isDelegate;
+        address[] targets;
+        uint256[] values;
+    }
+
+        
+    /// @notice Struct to hold parameters for the ERC20 swap operation
+    /// @param tokenIn The ERC20 token to swap from
+    /// @param tokenOut The ERC20 token to swap to
+    /// @param aggregator The address of the aggregator to use for the swap
+    /// @param swapData Additional data required for the swap
+    /// @param amountIn The amount of tokenIn to swap
+    /// @param minAmountOut The minimum amount of tokenOut expected from the swap
+    /// @param receiver The address that will receive the tokenOut
+    /// @param isDelegate Indicates if the swap is being executed by a delegate
+    /// @param targets Array of addresses to call during the swap for fees
+    /// @param calldataArray Array of calldata for each target for fees
+    struct SwapERC20Params {
+        IERC20 tokenIn;
+        IERC20 tokenOut;
+        address aggregator;
+        bytes swapData;
+        uint256 amountIn;
+        uint256 minAmountOut;
+        address receiver;
+        bool isDelegate;
+        address[] targets;
+        bytes[] calldataArray;
+    }
+
+    function swapERC20(SwapERC20Params calldata params) external;
+
+    function swapETH(SwapETHParams calldata params) external payable;
 }
