dbea8f41f8f9d0a8dca518591fa5645cb1ca5761
Jay Singh Dhakad <jaysingh20196@gmail.com>
2025-02-17 13:20:25 +0530

    feat: added fee distributor interface

---
+++ contracts/fee-distribution/interfaces/IFeeDistribution.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+interface IFeeDistribution {
+    event FeeDistributed(
+        address indexed feeToken,
+        bytes[] transactionHashes,
+        uint256[] amounts,
+        address[] receivers
+    );
+
+    function grantFeeDistributorRole(address account) external;
+
+    function revokeFeeDistributorRole(address account) external;
+
+    function distributeBatch(
+        address[] calldata feeTokens,
+        bytes[][] calldata transactionHashes,
+        uint256[][] calldata amounts,
+        address[][] calldata receivers
+    ) external;
+
+    function distribute(
+        address feeToken,
+        bytes[] calldata transactionHashes,
+        uint256[] calldata amounts,
+        address[] calldata receivers
+    ) external;
+}
